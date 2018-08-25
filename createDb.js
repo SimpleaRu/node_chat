7d2ed6f3b8563a78292dedd35076d52dd115b6d7
SimpleaRu <info@simplea.ru>
2018-08-25 12:26:05 +0300

    Add more functions to MongoClient

--- createDb.js
+++ createDb.js
@@ -14,9 +14,10 @@ MongoClient.connect(url, function(err, client) {
 
   const db = client.db(dbName);
 
-  insertDocuments(db, function() {
-    client.close();
-  });
+
+      removeDocument(db, function() {
+        client.close();
+      });
 });
 
 const insertDocuments = function(db, callback) {
@@ -32,4 +33,41 @@ const insertDocuments = function(db, callback) {
     console.log("Inserted 3 documents into the collection");
     callback(result);
   });
+}
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({'a': 3}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+}
+
+const removeDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ 'a' : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });    
 }
