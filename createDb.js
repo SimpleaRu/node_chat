767fae363475bf579ffc9e63c0c460a9b3111a25
SimpleaRu <info@simplea.ru>
2018-09-01 20:52:43 +0300

    Save first user

--- createDb.js
+++ createDb.js
@@ -1,13 +1,21 @@
 var User = require("./models/user").User;
+var mongoose = require('./lib/mongoose')
 
 var user = new User({
-  username: "Tester2",
+  username: "Tester",
   password: "secret"
 });
 
-user.save(function(err, user, affected) {
-  if (err) throw err;
-  User.findOne({ username: "Tester" }, function(err, tester) {
-    console.log(tester);
-  });
-});
+mongoose.connection.on('open', function () {
+  var db = mongoose.connection.db;
+  db.dropDatabase(function (err) {
+    if (err) throw err;
+    user.save(function (err, user, affected) {
+      if (err) throw err;
+      User.findOne({ username: "Tester" }, function (err, tester) {
+        console.log(tester);
+        mongoose.disconnect();
+      });
+    });
+  })
+})
