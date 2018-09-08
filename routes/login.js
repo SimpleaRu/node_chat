4cdf4ff7c89b43e32b1ab914ac352d9a6c4d79ce
SimpleaRu <info@simplea.ru>
2018-09-08 17:19:31 +0300

    Save user in authorization

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,35 @@
+const User = require('models/user').User;
+const async = require('async');
+var User = require("../models/user").User;
+var HttpError = require("../error").HttpError;
+
+
 exports.get = function(req, res) {
   res.render("login");
 };
+
+exports.post = function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+}
+
+async.waterfall([
+
+]);
+
+User.findOne({username: username}, function(err, user) {
+  if (err) return next(err)
+  if (user) {
+    if (user.checkPassword(password)) {
+      // 200
+    } else {
+      // 403
+    }
+  } else {
+    var user = new User({username: username, password: password});
+    user.save(function(err) {
+      if (err) return next(err);
+    })
+  }
+})
