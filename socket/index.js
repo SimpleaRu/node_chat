ea1c812f4b497de25bde057d72ded4b8edd1d800
SimpleaRu <info@simplea.ru>
2018-09-23 13:52:23 +0300

    Get userin socket.io

--- socket/index.js
+++ socket/index.js
@@ -2,27 +2,73 @@ var log = require("../lib/log")(module);
 var config = require("../config/config");
 var cookie = require('cookie');
 var cookieParser = require('cookie-parser');
+var sessionStore = require('../lib/sessionStore');
+const User = require('../models/user').User;
 
-function loadSession(sid, callback) {
-
+function loadSession(sid) {
+  return sessionStore.get(sid, function (err, session) {
+    return new Promise((resolve, reject) => {
+      if (!session) {
+        console.log('No session')
+        reject()
+      } else {
+        resolve(session);
+      }
+    })
+  });
 }
 
-module.exports = function(server) {
+function loadUser(session) {
+  return new Promise((resolve, reject) => {
+    if (!session.user) {
+      log.debug('Session is anonymos', session.id)
+      reject();
+    }
+    User.findById(session.user).exec((err, user) => {
+      if (err) return reject(err);
+      if (!user) reject();
+      resolve(user);
+    })
+  })
+}
+
+module.exports = function (server) {
   var io = require("socket.io").listen(server);
   io.set("origins", "localhost:*");
   io.set("logger", log);
 
-   io.use(function(socket, next) {
+  io.use(function (socket, next) {
     var handshakeData = socket.request
     handshakeData.cookies = cookie.parse(handshakeData.headers.cookie);
     var sidCookie = handshakeData.cookies[config.session.key];
     const sid = cookieParser.signedCookie(sidCookie, config.session.secret);
-    console.log(sid);
-    next();
+    if (!sid) {
+      return 'No sid find';
+    } else {
+      loadSession(sid)
+        .then(session => {
+          socket.handshake.session = session;
+          next();
+        })
+    }
   });
 
-  io.on("connection", function(socket) {
-    socket.on("message", function(text, cb) {
+  io.use(function (socket, next) {
+    loadUser(socket.handshake.session)
+      .then(user => {
+        socket.handshake.user = user;
+        next();
+      })
+    // console.log(socket.handshake);
+  })
+
+  io.use(function(socket, next) {
+    console.log(socket.handshake.user)
+    next();
+  })
+
+  io.on("connection", function (socket) {
+    socket.on("message", function (text, cb) {
       console.log(text);
       socket.broadcast.emit("message", text);
       cb(text);
